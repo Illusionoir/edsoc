42715b944dfc220ec6623129928e9d16d24fd8b6
Illusion <rohanthakur2684@gmail.com>
2025-10-02 14:42:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter basename="/edsoc"> 
+ReactDOM.render(
+  <BrowserRouter basename="/edsoc">
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
